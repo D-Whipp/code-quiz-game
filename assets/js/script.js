99e8d809be4a6e2b918621d9dae6cbe0f5d593ed
Mr Whipp <dwhipp88@gmail.com>
2021-07-11 19:38:30 -0600

    refactored css

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var startButton = document.querySelector("#start-btn");
+
+startButton.addEventListener()
+
+function startGame() {
+
+}
+
+function setNextQuestion() {
+    
+}
+
+function selectAnswer() {
+    
+}
