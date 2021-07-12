b9640303104e3885f5b25f3cb35c93b9126db1ec
Mr Whipp <dwhipp88@gmail.com>
2021-07-11 23:27:43 -0600

    added readme with image

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# fourth-bootcamp-challenge
+# Fourth Bootcamp Challenge: Code Quiz
+
+##
+
+<img src="code-quiz.png" />
+
+## Description
+
+Welcome to my fourth Coding Bootcamp Challenge! This one was real tough. It challenged me on another level and it's something I enjoyed.
+For this week I created a coding quiz from scratch. It was built utilizing vanilla JavaScript and a lot of Google/Youtube. I referenced previous lessons and class activities continuously for this challenge. I find myself in constant need of a deeper understanding of JavaScript and this challenge is yet another stepping stone bringing me closer to the goal: becoming a fluent JavaScript Developer.
+
+My motivation to complete this challenge comes from a desire to learn. With Coding in general we as developers are constantly learning. Not just the material but also ourselves and how we must teach ourselves.
+
+This project is challenging me to understand concepts. It's giving me the ability to look at something abstract and translate it in a tangible way.
+
+Coding Quiz stands out as a defiant challenger. It's not yet complete and I have a bit more to do before it'll become fully functional but I'll keep at it!
+
+## Installation
+
+N/A
+
+## Usage
+
+This application simply requires loading the browser and interacting with the on screen buttons.
+
+## Credits
+
+A HUGE shout out to Web Dev Simplified for his youtube video on [Building a Quiz App with JavaScript.](https://www.youtube.com/watch?v=riDzcEQbX6k&t=1332s)
+
+Also, I'd like to thank [MDN.](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+They're Docs are a true blessing for every developer and I'd be loss without them.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var questionEl = document.querySelector(".question");
+var answerBtnsEl = document.querySelector(".answer-buttons");
 var startBtnEl = document.querySelector("#start-btn");
 var nextBtnEl = document.querySelector("#next-btn");
-var answerBtnsEl = document.querySelector(".answer-buttons");
 var trueBtn = document.querySelector(".true-btn");
 var falseBtn = document.querySelector(".false-btn");
 var notifyHeadingEl = document.querySelector(".notify-h3");
@@ -10,32 +10,45 @@ var score = 0;
 
 // use array to story questions
 var questionArr = [
-  { q: "CSS stands for Cannot Stop Styling.", a: false },
-  { q: "CSS stands for Cascading Style Sheets.", a: true },
-  { q: "JavaScript cannot interact with HTML", a: false },
-  { q: "HTML lacks style.", a: true },
-  { q: "HTML lacks structure", a: false },
+  { q: "CSS stands for Cannot Stop Styling.", a: "false" },
+  { q: "CSS stands for Cascading Style Sheets.", a: "true" },
+  { q: "JavaScript cannot interact with HTML", a: "false" },
+  { q: "HTML lacks style.", a: "true" },
+  { q: "HTML lacks structure", a: "false" },
 ];
 
 function startGame() {
+  var currentIndex = 0;
+
   startBtnEl.classList.add("hide");
   answerBtnsEl.classList.remove("hide");
   nextBtnEl.classList.remove("hide");
-  questionEl.innerText = questionArr[1].q;
+  displayQuestion(currentIndex);
 }
 
 var answerHandler = function (e) {
-  var answer = e.target.innerText;
-  answer = answer.toLowerCase();
+  var answer = e.target.innerText.toLowerCase();
   console.log(answer);
-  return answer;
+  displayQuestion(answer);
 };
 
-var nextQuestion = function () {
-  
+var displayQuestion = function (a) {
+  answer = a;
+  var currentQuestion = questionArr[currentIndex].q;
+  questionEl.innerText = currentQuestion;
+
+  if (answer === questionArr[currentIndex].a) {
+    console.log("correct");
+  } else {
+    console.log("wrong");
+  }
+  currentIndex++;
+  displayQuestion();
 };
+
+// var nextQuestion = function (answer) {};
 
 trueBtn.addEventListener("click", answerHandler);
 falseBtn.addEventListener("click", answerHandler);
-nextBtnEl.addEventListener("click", nextQuestion);
+// nextBtnEl.addEventListener("click", nextQuestion);
 startBtnEl.addEventListener("click", startGame);
